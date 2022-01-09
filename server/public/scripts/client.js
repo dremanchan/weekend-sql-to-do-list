0482354ce5d0dec22d764e22c79cb4ce1d0523ec
Andre Manchanthasouk <dremanchan@gmail.com>
2022-01-08 23:44:37 -0600

    created todo object

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,13 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQ sourced');
-    
+    $(document).on('click', '.newTaskButton', submitTask);
+}
+
+function submitTask() {
+    console.log('submit button clicked', submitTask);
+    let todo = {
+        task: "",
+        status: ""
+    };
 }
