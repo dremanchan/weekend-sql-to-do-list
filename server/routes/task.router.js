988c98b513a3b8ae97a7ee44e111848aa36fcd8e
Andre Manchanthasouk <dremanchan@gmail.com>
2022-01-09 01:07:03 -0600

    get and post endpoint created in router.

---
+++ server/routes/task.router.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+// GET endpoint
+router.get('/', (req, res) => {
+    let queryText = `SELECT * FROM "tasks"`;
+    pool.query(queryText)
+        .then((dbRes) => {
+            res.send(dbRes.rows);
+        })
+        .catch((err) => {
+            console.log('GET /tasks failed', err);
+            res.send(500);
+        });
+});
+
+// POST endpoint
+
+router.post('/', (req, res) => {
+    console.log('req.body is', req.body);
+    let queryText = `
+        INSERT INTO "tasks"
+            ("task")
+        VALUES
+            ($1)
+    `;
+    let queryParams = [
+        req.body.task
+    ];
+    console.log('queryText is', queryText);
+    pool.query(queryText, queryParams)
+        .then((dbRes) => {
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log('POST failed', err);
+            res.sendStatus(500);
+        })
+});
